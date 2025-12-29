bd8cc853d4960341417da8eb621ad3764d4df781
abnvgurram <abnv.gurram2905@gmail.com>
2025-12-30 01:54:47 +0530

    refactor: split required-sql.sql into module-specific sql files

---
+++ admin/sql/sql_general.sql
@@ -0,0 +1,10 @@
+-- ============================================
+-- CRAFTSOFT ADMIN DATABASE SCHEMA
+-- General Verification Queries
+-- ============================================
+
+-- SELECT * FROM courses;
+-- SELECT * FROM tutors;
+-- SELECT * FROM students;
+-- SELECT * FROM activities;
+-- SELECT * FROM inquiries;
